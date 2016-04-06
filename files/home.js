5e70776bbe84508c652ffafe0ad279cd072e104c
Gervásio Júnior <juniorb08@gmail.com>
2016-04-06 14:39:26 -0300

    ..

---
+++ files/home.js
@@ -0,0 +1,26 @@
+/*Template.HOME.events({
+
+    "click sendmail": function(e, t) {
+        var email = t.find('#email').value.trim();
+        var name = t.find('#name').value.trim();
+        var msg = t.find('#msg').value;
+
+        if (Meteor.isServer) {
+            Email.send({
+                from: email,
+                to: "pracadasprofissoes@gmail.com",
+                subject: "Formulário de Contato: " + name,
+                text: msg + 
+                "\n\n----------------------------------------------------------\n" +
+                "Enviado por " + name + " <" + email + ">\n" +
+                new Date().toLocaleString()
+            });
+        }
+    }
+});*/
+Template.TEMPLATE_NAME.events({
+
+});
+
+Template.TEMPLATE_NAME.helpers({
+});
