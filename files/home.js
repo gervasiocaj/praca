a006340eaf6dbbc59cb12b3736e3990b2e623510
Gervásio Júnior <juniorb08@gmail.com>
2016-04-06 22:46:59 -0300

    avanço

--- files/home.js
+++ files/home.js
@@ -1,25 +1,21 @@
-/*Template.HOME.events({
-
-    "click sendmail": function(e, t) {
-        var email = t.find('#email').value.trim();
-        var name = t.find('#name').value.trim();
-        var msg = t.find('#msg').value;
-
-        if (Meteor.isServer) {
-            Email.send({
-                from: email,
-                to: "pracadasprofissoes@gmail.com",
-                subject: "Formulário de Contato: " + name,
-                text: msg + 
-                "\n\n----------------------------------------------------------\n" +
-                "Enviado por " + name + " <" + email + ">\n" +
-                new Date().toLocaleString()
-            });
-        }
-    }
-});*/
 Template.TEMPLATE_NAME.events({
+  "click sendmail": function(e, t) {
+    var email = t.find('#email').value.trim();
+    var name = t.find('#name').value.trim();
+    var msg = t.find('#msg').value;
 
+    if (Meteor.isServer) {
+      Email.send({
+        from: email,
+        to: "pracadasprofissoes@gmail.com",
+        subject: "Formulário de Contato: " + name,
+        text: msg + 
+        "\n\n----------------------------------------------------------\n" +
+        "Enviado por " + name + " <" + email + ">\n" +
+        new Date().toLocaleString()
+      });
+    }
+  }
 });
 
 Template.TEMPLATE_NAME.helpers({
